0574c62ab2023e47c5938502986aa481da4a6440
muzaffarhaque <muzaffarhaque777@gmail.com>
2023-01-11 13:00:35 +0530

    add Image

--- src/App.js
+++ src/App.js
@@ -1,50 +1,22 @@
 
 import { useEffect, useState } from 'react';
 import './App.scss';
+import table1 from "./images/Table.jpg"
 function App() {
 const [car,setCar]=useState("");
 const [ckbox,setCkbox]=useState([]);
-const [fulform,setFulform]=useState({BS01:"",EF01:"",CF01:"",BF01:"",GF01:""});
+// const [fulform,setFulform]=useState({BS01:"",EF01:"",CF01:"",BF01:"",GF01:""});
 const [finalObj,setFinalObj]=useState([]);
-const [totalbil,setTotalbil]=useState(0)
+const [totalbil,setTotalbil]=useState(0);
+const [chk,setChk]=useState({BS01:false,EF01:false,CF01:false,BF01:false,GF01:false});
+const {BS01,EF01,CF01,BF01,GF01}=chk;
 const [price,setPrice]=useState({
 "BS01":2000,
 "EF01":5000,
 "CF01":2000,
 "BF01":1000,
 "GF01":3000});
-const {BS01,EF01,CF01,BF01,GF01}=fulform;
-const service={
-   BS01:"Basic Servicing",
-   EF01:"Engine Fixing",
-   CF01:"Clutch Fixing ",
-   BF01:"Brake Fixing ",
-   GF01:"Gear Fixing"
-}
-// const hatchback=
-//   {
-//    "BS01":2000,
-//    "EF01":5000,
-//    "CF01":2000,
-//    "BF01":1000,
-//    "GF01":3000
-//   }
-// let sedan=
-//   {
-//    "BS01":4000,
-//    "EF01":8000,
-//    "CF01":4000,
-//    "BF01":1500,
-//    "GF01":6000
-//   }
-// let suv=
-//   {
-//    "BS01":5000,
-//    "EF01":10000,
-//    "CF01":6000,
-//    "BF01":2500,
-//    "GF01":8000
-//   }
+// const {BS01,EF01,CF01,BF01,GF01}=fulform;
 
 
 
@@ -55,31 +27,35 @@ const service={
     
     if(checked){
       //user checked box
-      setCkbox([...ckbox,value])
-      setFulform({...fulform,[e.target.value]:name});
+      setCkbox([...ckbox,value]);
+      // setFulform({...fulform,[e.target.value]:name});
+      // setChk({...chk,ck1:checked,ck2:checked,ck3:checked,ck4:checked,ck5:checked});
+      setChk({...chk,[value]:checked})
       setFinalObj([...finalObj,{pri:id,value:name}]);
       setTotalbil(totalbil + parseInt(id));
-      // console.log(id,"jjjj")
+      // console.log(chk,"jjjj")
      
     }else{
       //user unchedk boxx
       setCkbox(ckbox.filter((e)=>e!==value));
-      setFulform({...fulform,[e.target.value]:""});
+  
+      // setFulform({...fulform,[e.target.value]:""});
+      setChk({...chk,[value]:false})
       setFinalObj(finalObj.filter((e)=>e.value!==name));
       setTotalbil(totalbil - parseInt(id));
     }
   }
 
-useEffect(
-  ()=>{
+// useEffect(
+//   ()=>{
    
-    //  console.log("Name :",fulform);
-     console.log("Name :",price.BS01);
-    //  console.log("Price diff Type care",price)
-     console.log("Final Object",finalObj)
+//      console.log("Name :",fulform);
+//      console.log("Name :",price.BS01);
+//      console.log("Price diff Type care",price)
+//      console.log("Final Object",finalObj)
      
-  },[ckbox]
-)
+//   },[ckbox]
+// )
 useEffect(
   ()=>{
 
@@ -94,7 +70,7 @@ useEffect(
           "GF01":3000
          })
         break;
-      case "Sed":
+      case "Sedan":
         setPrice( {
           "BS01":4000,
             "EF01":8000,
@@ -131,51 +107,54 @@ useEffect(
 function select_car(e){
 
 setCar(e.target.value);
-
-
-
+setTotalbil(0);
+setChk({BS01:false,EF01:false,CF01:false,BF01:false,GF01:false});
+setFinalObj([]);
+setCkbox([]);
 }
   return (
     <div className="App">
+      <img src={table1} alt="table data" className='table-image-details' />
    <div className="child1">
-
          <select name="type_of_care" id="type"  onChange={select_car} className="select-option">
-          <option selected value="">select Type of care</option>
+          <option selected value="">select Type of car</option>
           <option value="Hatchback">Hatchback</option>
-          <option value="Sed">Sedan </option>
+          <option value="Sedan">Sedan </option>
           <option value="Suv">Suv</option>
          </select>
 
    </div>
 
   
-
-   <div className="slect-service">
-     
-      <label htmlFor="Basic Servicing">
-        <input type="checkbox" value="BS01" pric="6666" onChange={getdata} name="Basic Servicing" id={price.BS01} />
-        BS01
-      </label>
-      <label htmlFor="Engine Fixing">
-        <input type="checkbox" value="EF01"  pric={price.EF01} onChange={getdata} name="Engine Fixing" id={price.EF01} />
-        EF01
-      </label>
-      <label htmlFor="Clutch Fixing">
-        <input type="checkbox" value="CF01"  pric={price.CF01} onChange={getdata} name="Clutch Fixing" id={price.CF01} />
-        CF01
-      </label>
-      <label htmlFor="Brake Fixing">
-        <input type="checkbox" value="BF01"  pric={price.BF01} onChange={getdata} name="Brake Fixing" id={price.BF01} />
-        BF01
-      </label>
-      <label htmlFor="Gear Fixing" title='Gear Fixing'>
-        <input type="checkbox" title='Gear Fixing'  pric={price.GF01} value="GF01" onChange={getdata} name="Gear Fixing" id={price.GF01} />
-        GF01
-      </label>
-   </div>
+{
+  car!=""?<div className="slect-service">
+     <center><h2>Service</h2></center>
+  <label htmlFor="Basic Servicing">
+    <input type="checkbox" value="BS01" pric="6666" onChange={getdata} name="Basic Servicing" checked={BS01} id={price.BS01} />
+    BS01
+  </label>
+  <label htmlFor="Engine Fixing">
+    <input type="checkbox" value="EF01"  pric={price.EF01} onChange={getdata} name="Engine Fixing" checked={EF01} id={price.EF01} />
+    EF01
+  </label>
+  <label htmlFor="Clutch Fixing">
+    <input type="checkbox" value="CF01"  pric={price.CF01} onChange={getdata} name="Clutch Fixing" checked={CF01} id={price.CF01} />
+    CF01
+  </label>
+  <label htmlFor="Brake Fixing">
+    <input type="checkbox" value="BF01"  pric={price.BF01} onChange={getdata} name="Brake Fixing" checked={BF01} id={price.BF01} />
+    BF01
+  </label>
+  <label htmlFor="Gear Fixing" title='Gear Fixing'>
+    <input type="checkbox" title='Gear Fixing'  pric={price.GF01} value="GF01" onChange={getdata} name="Gear Fixing" checked={GF01} id={price.GF01} />
+    GF01
+  </label>
+</div>:<center><h2>⬆️ Select Car Type ⬆️</h2></center>
+}
+   
    <div className="disply-output">
     <h4> Service Bill :</h4>
-    <h3>Type of Care <span>-{car}</span></h3>
+    <h3>Type of Car <span>-{car}</span></h3>
     <h3>Service Code <span>-{ckbox.join(" , ")}</span></h3><br/>
 
 {
@@ -191,6 +170,7 @@ setCar(e.target.value);
     
     <h3>Total Bill <span>- {totalbil}</span>{totalbil>=10000?<span className='Notes'> ( Will provide free cleaning )</span>:null}</h3>
    </div>
+  {/* <p style={{fontSize:"20px" ,height:"200px"}}>tall est height</p> */}
     </div>
   );
 }
